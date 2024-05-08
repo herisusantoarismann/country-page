d43b8338bf2fd2007a45a5340b4461c30a028494
heri.s.arisman@gmail.com <heri.s.arisman@gmail.com>
2024-05-08 17:16:53 +0700

    default: base url api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL_API,
+  baseURL:
+    process.env.NEXT_PUBLIC_BASE_URL_API || "https://restcountries.com/v3.1",
   headers: {
     "Content-Type": "application/json",
   },
